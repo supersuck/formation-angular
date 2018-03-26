55a069482e8bb9fdf468b965425846f24d54079f
supersuck <mathieuouyang@gmail.com>
2018-03-26 15:16:23 +0200

    create module core & shared

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    CoreModule,
+    SharedModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
