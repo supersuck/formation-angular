1b4200fb58ad7f59dd6bc565c8ccb7a71e584657
supersuck <mathieuouyang@gmail.com>
2018-03-28 14:29:44 +0200

    'subscribe

--- my-app/src/app/items/containers/list-items/list-items.component.ts
+++ my-app/src/app/items/containers/list-items/list-items.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Item } from '../../../shared/models/item.model';
 import { COLLECTION } from '../../../core/services/collection';
 import { CollectionService } from '../../../core/services/collection/collection.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-list-items',
   templateUrl: './list-items.component.html',
   styleUrls: ['./list-items.component.css']
 })
-export class ListItemsComponent implements OnInit {
-  collection: Item[];
+export class ListItemsComponent implements OnInit, OnDestroy {
+  collection$: Observable<Item[]>;
+  sub: Subscription;
   constructor(private collectionService: CollectionService) {
 
   }
 
   ngOnInit() {
-    this.collectionService.collection$.subscribe((data) => {
-      console.log(data);
-      this.collection = data;
-    });
+    // this.sub = this.collectionService.collection$.subscribe((data) => {
+    //   this.collection = data;
+    // });
+    this.collection$ = this.collectionService.collection$;
   }
 
+  ngOnDestroy() {
+    // this.sub.unsubscribe();
+  }
 }
